10e3dd0a2a999ba145da6f0d74107605a1b0196e
mo-ri-regen <moricontb@gmail.com>
2021-05-30 15:43:48 +0900

    🎨 Header部分コンポーネント化

---
+++ components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+
+export function Header(){
+  return(
+    <>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <p className={styles.description}>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/index.js</code>
+      </p>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import {Header} from '../components/Header/'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,14 +14,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      
+        <Header />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
