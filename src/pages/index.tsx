04a55d9cd9baf6ce6b650ae370b1e84157a80878
mo-ri-regen <moricontb@gmail.com>
2021-05-31 00:53:00 +0900

    ✨ count up機能追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
+import {useState} from 'react'
 
 import {Header} from '../components/Header'
 import {Footer} from '../components/Footer'
+import React from 'react'
 
 export default function Home() {
+
+  const [count, setCount] = useState(0);
+
+  const CountUp = () =>{
+    setCount(count+1);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,33 +27,11 @@ export default function Home() {
         <Header />
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+ 
+         <button onClick={CountUp}>カウントアップ</button>
+         <div>
+            {count}
+          </div>
         </div>
       </main>
 
