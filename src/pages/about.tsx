973ae769cee7b5f93c2121bbf3d1e55386ed5f1b
mo-ri-regen <moricontb@gmail.com>
2021-06-01 06:37:07 +0900

    ✨ aboutページでもカウントできるように修正。

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,26 @@ import {Header} from '../components/Header'
 import {Main} from '../components/Main'
 import {Footer} from '../components/Footer'
 
+import {useState} from 'react'
+
 export default function About() {
+
+  const [count, setCount] = useState<number>(0);
+
+  const handleCountUp = () =>{
+    setCount(count+1);
+  }
+  const handleCountDown = () =>{
+    if(count > 0){
+      setCount(count-1);
+    }
+  }
 
   return(
     <div>
       <main>
         <Header />
-        <Main />
+        <Main count={count} CountUp={handleCountUp} CountDown={handleCountDown}/>
       </main>
       <Footer />
     </div>
