5f97fc50ad72db3b7808b8ccf50e779298fced0b
mo-ri-regen <moricontb@gmail.com>
2021-05-31 22:02:38 +0900

    💄 Aboutページ追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
 import styles from './Header.module.css'
+import Link from 'next/link'
 
 export const Header = () =>{
   return(
-    <>
+    <div>
       <h1 className={styles.title}>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
@@ -11,6 +12,15 @@ export const Header = () =>{
         Get started by editing{' '}
         <code className={styles.code}>pages/index.js</code>
       </p>
-    </>
+      <div className={styles.list}>
+        <li>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link href={'/about'}>About</Link>
+        </li>
+        
+      </div>
+    </div>
   );
 }
---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import {Header} from '../components/Header'
+import {Main} from '../components/Main'
+import {Footer} from '../components/Footer'
+
+export default function About() {
+
+  return(
+    <div>
+      <main>
+        <Header />
+        <Main />
+      </main>
+      <Footer />
+    </div>
+    
+  );
+}
