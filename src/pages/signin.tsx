6f5a8b5a19409ee4d9228915c056c543a77536ae
mo-ri-regen <moricontb@gmail.com>
2021-06-12 23:05:36 +0900

    🌱 add signin page

---
+++ src/pages/signin.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+export default function SignIn() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
