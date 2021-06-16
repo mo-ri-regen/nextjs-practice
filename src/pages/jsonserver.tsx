bbc5d8b9e4acc5fbf1c1a033084fe6ffee741950
mo-ri-regen <moricontb@gmail.com>
2021-06-16 21:39:10 +0900

    💄 api叩く用のページ追加

---
+++ src/pages/jsonserver.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+export default function jsonserver(){
+  return(
+    <div>
+      <Button variant="contained" color="primary" size="small">getdata</Button>
+    </div>
+  );
+}
