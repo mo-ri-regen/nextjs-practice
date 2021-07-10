546b069eb423e486fb3af5eb72edb9e6af2884c1
mo-ri-regen <moricontb@gmail.com>
2021-07-10 12:05:57 +0900

    ⚡️getstaticPropsでデータ取得を行なった

--- src/pages/jsonserver.tsx
+++ src/pages/jsonserver.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { useState, useCallback, useEffect } from "react";
 import Button from "@material-ui/core/Button";
-import Link from "next/link"
+import Link from "next/link";
+import { GetStaticProps } from "next";
 
-export default function JsonServer() {
-  const [posts, setPosts] = useState([]);
-  const fetchData = useCallback(async () => {
-    try {
-      const res = await fetch("http://localhost:3004/posts");
-      const json = await res.json();
-      setPosts(json);
-    } catch {
-      alert("error");
-    }
-  }, []);
+export default function JsonServer({ posts }) {
+  // const [posts, setPosts] = useState([]);
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     const res = await fetch("http://localhost:3004/posts");
+  //     const json = await res.json();
+  //     setPosts(json);
+  //   } catch {
+  //     alert("error");
+  //   }
+  // }, []);
   const handleClick = () => {
-    fetchData();
+    // fetchData();
   };
 
   // useEffect(() => {
@@ -28,18 +29,30 @@ export default function JsonServer() {
         variant="contained"
         color="primary"
         size="small"
-        onClick={handleClick}
+        // onClick={handleClick}
       >
         getdata
       </Button>
-        {posts.map((post) => {
-          return( 
-            <Link key={post.id} href={`/post/${post.id}`}>
-              <a>
-              {post.title}
-              </a>
-            </Link>);
-        })}
+      {posts.map((post) => {
+        return (
+          <Link key={post.id} href={`/post/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        );
+      })}
     </div>
   );
 }
+// ビルド時 getstaticprops(SSG)
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const res = await fetch("http://localhost:3004/posts");
+    const posts = await res.json();
+
+    return {
+      props: { posts },
+    };
+  } catch {
+    alert("error");
+  }
+};
