74d7a3192217a894ea266d19dd2fea312cb1b96c
mo-ri-regen <moricontb@gmail.com>
2021-07-15 23:41:48 +0900

    💩戻る機能だけ追加

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -2,8 +2,8 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Post() {
-  // const router = useRouter();
-  // const { pid } = router.query;
+  const router = useRouter();
+  const { pid } = router.query;
 
   // return <p>Post: {pid}</p>;
   return (
@@ -12,4 +12,11 @@ export default function Post() {
         <a>戻る</a>
       </Link>
     </div>
+  );
 }
+// export async function getStaticPaths() {
+//   return { pid };
+// }
+// export async function getStaticProps({ params }) {
+//   const res = await fetch(`https://localhost/posts/${params.id}`);
+// }
