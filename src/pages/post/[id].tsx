088769a04170a110f4ee32c44f1a3566ee2c94ac
mo-ri-regen <moricontb@gmail.com>
2021-07-15 22:52:14 +0900

    ♻️prettierによるリファクタリング

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,19 +1,15 @@
 import { useRouter } from "next/router";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Post() {
   // const router = useRouter();
   // const { pid } = router.query;
 
   // return <p>Post: {pid}</p>;
-  return(
+  return (
     <div>
       <Link href="/jsonserver">
-        <a>
-          戻る
-        </a>
+        <a>戻る</a>
       </Link>
-      Hello
     </div>
-  )
 }
